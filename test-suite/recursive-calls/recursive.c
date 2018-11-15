fbe5118c23ea79510e3afae75e264565c77f5610
shen_liu <sxl463@gmail.com>
2018-11-14 19:07:54 -0500

    update

--- test-suite/recursive-calls/recursive.c
+++ test-suite/recursive-calls/recursive.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <string.h> 
 
-
 int auth(char c){
-  if(c == 'd')
+  if(c == 'z')
     return 1;
   else
     return auth(c+1);
 }
 
 
+
 int main() {
-    FILE *fp;
-    char* filename = "password.txt";
+  FILE *fp;
+  char* filename = "password.txt";
 
-    fp = fopen(filename, "r");
-    char input = fgetc(fp);//read a char from the file
+  fp = fopen(filename, "r");
+  char input = fgetc(fp);//read a char from the file
 
+  //  for (int i = 0; i < 100; i++){
     if(auth(input))
       printf("YES\n");
     else
       printf("NO\n");
-
-    fclose(fp);
-    return 0;
+    // }
+  fclose(fp);
+  return 0;
 }
 
